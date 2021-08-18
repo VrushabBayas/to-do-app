e193941c7939f7a1aaf233527abb64ce6b91314c
Vrushab Bayas <vrushab.bayas@gmail.com>
2021-08-18 18:37:42 +0530

    code refactored

--- src/Components/ToDos/Components/TodoList.js
+++ src/Components/ToDos/Components/TodoList.js
@@ -1,40 +1,20 @@
 /* eslint-disable react/forbid-prop-types */
 import React from "react";
 import PropTypes from "prop-types";
-import { v4 as uuidv4 } from "uuid";
-import { Paper, makeStyles, Checkbox, Button } from "@material-ui/core";
-import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import { makeStyles, Button } from "@material-ui/core";
+
 import {
-  DeleteFilled,
   SortAscendingOutlined,
   SortDescendingOutlined,
 } from "@ant-design/icons";
 
 import strings from "../../../Utils/Constants";
+import Todo from "./Todo";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     padding: theme.spacing(2),
-  },
-  paper: {
-    padding: theme.spacing(1),
-    marginBottom: theme.spacing(2),
-    color: theme.palette.text.secondary,
-    display: "flex",
-    justifyContent: "start",
-    alignItems: "center",
-  },
-  item: {
-    marginLeft: theme.spacing(1),
-    display: "flex",
-  },
-  itemComplete: {
-    marginLeft: theme.spacing(1),
-    textDecoration: "line-through",
-  },
-  buttons: {
-    margin: "auto",
   },
   titleWrapper: {
     display: "flex",
@@ -87,43 +67,17 @@ function TodoList({
   const handleOnDeleteKey = (id) => {
     deleteTodo(id);
   };
+
   const getTodoList = () => {
     return todoList.map((todo, index) => {
       return (
-        <Paper className={classes.paper} key={uuidv4()}>
-          <Checkbox
-            color="primary"
-            inputProps={{ "aria-label": "secondary checkbox" }}
-            checked={todo.complete}
-            onChange={() => handleTodoClick(todo.id)}
-          />
-          <div className={todo.complete ? classes.itemComplete : classes.item}>
-            {todo.title}
-          </div>
-          <div className={classes.item}>
-            <div className={classes.buttons}>
-              <Button
-                disabled={index === 0}
-                onClick={() => handleOnArrowKey(strings.UP, todo.id)}
-              >
-                <ArrowUpward />
-              </Button>
-            </div>
-            <div className={classes.buttons}>
-              <Button
-                disabled={index === todoList.length - 1}
-                onClick={() => handleOnArrowKey(strings.DOWN, todo.id)}
-              >
-                <ArrowDownward />
-              </Button>
-            </div>
-            <div className={classes.buttons}>
-              <Button onClick={() => handleOnDeleteKey(todo.id)}>
-                <DeleteFilled />
-              </Button>
-            </div>
-          </div>
-        </Paper>
+        <Todo
+          todo={todo}
+          index={index}
+          handleOnDeleteKey={handleOnDeleteKey}
+          handleOnArrowKey={handleOnArrowKey}
+          handleTodoClick={handleTodoClick}
+        />
       );
     });
   };
